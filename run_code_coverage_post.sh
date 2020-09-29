e187b043d1936a6291f0882277d231007e4f15ea
hurry.qin <hurry.qin@dji.com>
2020-09-29 19:39:55 +0800

    专门用途
    专门用途

--- run_code_coverage_post.sh
+++ run_code_coverage_post.sh
@@ -3,4 +3,4 @@
 #   Copyright 2020 Quality Coding, Inc. See LICENSE.txt
 
 scripts="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-"${scripts}/getcov" --show
+"${scripts}/getcov" -o $CI_PROJECT_BUILD_RESULT_PATH
